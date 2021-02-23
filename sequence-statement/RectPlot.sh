7fc149c8cfeb789ced12e47bafa4e2e4425467db
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-02-23 23:54:28 +0530

    added: rectangular plot and into acres conv

--- sequence-statement/RectPlot.sh
+++ sequence-statement/RectPlot.sh
@@ -8,3 +8,8 @@ result=$(($feet1*$feet2))
 result1=$(($result/3))
 
 echo "The result for" $feet1*$feet2 "in meters is:" $result1
+
+result3=$((25*$result1))
+result4=$(($result3*43560))
+
+echo  25 plots are converted into $result4 acres

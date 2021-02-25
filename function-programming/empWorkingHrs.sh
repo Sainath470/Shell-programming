16b899ab50b96cbcda4d18f6c62655785ae4c4b0
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-02-25 23:03:20 +0530

    Added:Palindrome for two inputs

---
+++ function-programming/empWorkingHrs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+#constant
+IS_FULLTIME=1
+IS_PARTTIME=2
+EMP_RATE_PER_HR=20
+Num_of_working_days=15
+max_hrs_in_month=20
+
+#variable
+total_W_D=0
+totalEMP_HRS=0
+Function getworkingHrs(){
+	 case $1 in
+      $IS_FULLTIME)
+      empHrs=8
+      ;;
+      $IS_PARTTIME)
+      empHrs=4
+      ;;
+      *)
+      empHrs=0
+      ;;
+   esac
+	echo $empHRS
+
+}
+while [[ $totalEMP_HRS -lt $max_hrs_in_month && $totalworkingdays -lt $num_working_days ]]
+do
+	((totalworkingdays++))
+	empCheck=$((RANDOM%3))
+	empHrs=$( getworkingHrs $empCheck)
+	
+done
+
---
+++ function-programming/palindrome.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+#palidrome checking
+read -p "Enter the first number you want to check : " num
+read -p "Enter the second number you want to check :" num2
+s=0
+rev=" "
+temp=$num
+
+
+function palindrome_check()
+{
+while [ $num -gt '0' ]
+do
+	s=$(($num%10))
+	num=$(($num/10))
+	rev=$(echo ${rev}${s})
+done
+if [ $temp -eq $rev ]
+then
+   echo $temp "is a palindrome"
+else
+   echo $temp "is not a palindrome"
+fi
+}
+palindrome_check
+
+num=$num2
+temp=$num2
+palindrome_check

dea560e64c795b56c16e96cda488859209fca579
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-01 00:57:47 +0530

    Added: sum and average of 5 random numbers

--- sequence-statement/Random.sh
+++ sequence-statement/Random.sh
@@ -7,7 +7,7 @@ ran4=$((9+RANDOM%98))
 ran5=$((9+RANDOM%98))
 
 result=$(($ran1+$ran2+$ran3+$ran4+$ran5))
-Average=$((result/5))
+Average=$(($result/5))
 echo "first number is:"$ran1
 echo "second number is:"$ran2
 echo "third number is:"$ran3
